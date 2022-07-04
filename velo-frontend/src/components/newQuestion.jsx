a5c51597ffc937eef7b80c75d2543e30be3a31d7
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-07-04 09:48:43 +0100

    Update new question component

--- velo-frontend/src/components/newQuestion.jsx
+++ velo-frontend/src/components/newQuestion.jsx
@@ -1,31 +1,56 @@
-import {useState} from 'react'
+import {useState, useMemo} from 'react'
 import "../styles/question.css"
+import {useCreatePost} from '../adapters/post'
+import {notEmptyString, uid} from '../utils'
 
 const NewQuestion = ({setShowQuestion}) => {
+    const create_post = useCreatePost()
 
     const [question, setQuestion] = useState('')
     const [topics, setTopics] = useState('')
+
+    const topicsarr = useMemo(()=>{
+        return topics.split(/[ ,]+/);
+    },[topics])
+
     const remove = () => {
         setShowQuestion(false)
     }
+    const setQuestionText=()=>{
+
+    }
+    const uploadQuestion=()=>{
+        if(notEmptyString(question) ** notEmptyString(topics)){
+            create_post(question, topics)
+        }
+    }
 return (
-    <div className=''>
-    <div className=''>
-        <div className='bg-[white] h-[35%] fixed w-full py-10 rounded-t-[80px] md:rounded-3xl md:w-96 px-6 md:left-0 md:right-0 md:top-0 md:m-auto bottom-0 box'>
+    <div>
+        <div className='bg-[white] h-[50%] fixed w-full py-10 rounded-t-[80px] md:rounded-3xl md:w-96 px-6 md:left-0 md:right-0 md:top-0 md:m-auto bottom-0 box'>
             <div className='pb-5'>
                 <svg onClick={remove} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:cursor-pointer text-white absolute right-10 top-8" fill="#f9f9f9" viewBox="0 0 24 24" stroke="black" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
             </div>
             <input type='text' className='bg-transparent border-b w-full py-2 outline-none' placeholder='Type a new question...'  vakue={question} onChange={e=>setQuestion(e.target.value)}/>
-            <h2 className='font-medium text-sm mt-5'>Topic?</h2>
-            <div className='flex md:block'>
-            <input type='text' className='bg-transparent border-b w-full py-2 outline-none' placeholder='Set topics...' value={topics} onChange={e=>setTopics(e.target.value)}/>
 
-                <div className='md:mt-6 absolute right-4'>
+
+            <h2 className='font-medium text-sm mt-5'>Topic?</h2>
+            <section className='newquestion__topicsarr'>
+            {
+                topicsarr.map(item => <div key={uid()}>
+                    <div className='newquestion__topic'> {item}</div>
+                </div>)
+            }
+            </section>
+
+
+            <div className='flex md:block'>
+            <input type='text' className='bg-transparent border-b w-full py-2 outline-none' placeholder='Set topics...' value={topics} onChange={(e)=>setTopics(e.target.value)}/>
+
+                <div className='md:mt-6 absolute right-4' onClick={uploadQuestion}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-[#003566] rounded-full p-2 rotate-45 bg-gray-200 hover:cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" /></svg>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 )
 }
