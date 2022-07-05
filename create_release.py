1c10a2791a81952d9b3d10bd7fd70482f8911484
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-07-05 05:04:34 +0100

    Add create release script

--- create_release.py
+++ create_release.py
@@ -1,1 +1,67 @@
+# FOR CREATING RELEASE
+
 import os
+import shutil
+
+
+rootbackend_folder= 'velo-backend'
+rootfrontend_folder= 'velo-frontend'
+
+files = ['manage.py', 'Procfile', 'requirements.txt', '.env']
+
+folders = [ dir for dir in os.listdir(rootbackend_folder) if os.path.isdir(os.path.join(rootbackend_folder, dir)) ]
+
+print('Deleting folders')
+for folder in folders:
+    try:
+        shutil.rmtree(folder)
+    except:
+        pass
+
+print('Copying folders')
+for folder in folders:
+    try:
+        shutil.copytree(f'./{rootbackend_folder}/{folder}', folder)
+    except:
+        pass
+
+print('Copying files')
+for file in files:
+    try:
+        shutil.copyfile(f'./{rootbackend_folder}/{file}', file)
+    except:
+        pass
+
+
+try:
+    os.chdir('velo-frontend')
+    print('Running build')
+    os.system('npm run build')
+    os.chdir('..')
+except:
+    pass
+
+try:
+    os.mkdir('./velo/dist')
+except:
+    pass
+
+
+try:
+    print('copying index.html')
+    shutil.copyfile(f'./{rootfrontend_folder}/dist/index.html', './velo/dist/index.html')
+except:
+    pass
+
+try:
+    print('copying static folder')
+    shutil.copytree(f'./{rootfrontend_folder}/dist/static', './accounts/templates/static')
+except:
+    pass
+
+
+with open ('.gitignore', 'a') as ignore:
+    ignore.write('\n')
+    ignore.write(f'/{rootbackend_folder}')
+    ignore.write('\n')
+    ignore.write(f'/{rootfrontend_folder}')
