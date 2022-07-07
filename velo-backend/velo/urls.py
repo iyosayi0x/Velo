2b45db9d55c14f976c8c401f67a5c77d7878436b
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-07-07 12:29:13 +0100

    Update

--- velo-backend/velo/urls.py
+++ velo-backend/velo/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/lessons/', include('lesson.urls')),
 ]
 
+urlpatterns += [re_path(r'.*', TemplateView.as_view(template_name='index.html'))]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
